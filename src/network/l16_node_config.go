71bc1266bf1375c8edcb135f177f805721e4a417
Leonard Schellenberg <leonard.schellenberg@gmail.com>
2022-05-06 15:51:32 +0200

    new network init routine

--- src/network/l16_node_config.go
+++ src/network/l16_node_config.go
@@ -24,14 +24,14 @@ var DefaultL16NodeConfigs = &NodeConfigs{
 		Etherbase:    "0x7781121fd00A009670E31b76A2bf99b3A2D6878D",
 		DataVolume:   "./data/execution_data",
 		NetworkId:    "83748374",
-		Bootnode:     "enode://c2bb19ce658cfdf1fecb45da599ee6c7bf36e5292efb3fb61303a0b2cd07f96c20ac9b376a464d687ac456675a2e4a44aec39a0509bcb4b6d8221eedec25aca2@35.204.5.43:30303",
+		Bootnode:     "enode://c2bb19ce658cfdf1fecb45da599ee6c7bf36e5292efb3fb61303a0b2cd07f96c20ac9b376a464d687ac456675a2e4a44aec39a0509bcb4b6d8221eedec25aca2@34.91.51.22:30303\n",
 	},
 	Consensus: &ClientDetails{
 		StatsAddress: "34.141.143.70:9090",
 		Verbosity:    "info",
-		Version:      "v0.2.1-dev",
+		Version:      "v0.2.2-dev",
 		DataVolume:   "./data/consensus_data",
-		Bootnode:     "enr:-MK4QOoOAELWWC0dZ7hwZzDY3NhxbGJWB9JFBGsIswzF383NRPNh7vfI_K4gt5KMCFt6-NrMbUdizURmcKE5xjfRhBaGAYBwAjI7h2F0dG5ldHOIAAAAAAAAAACEZXRoMpAMEg0LYQAAcAMAAAAAAAAAgmlkgnY0gmlwhCPMBSuJc2VjcDI1NmsxoQO4XSsbls7lyhfqvcsgS8jmjFmBpC3dekXssvAEXkHtJYhzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A",
+		Bootnode:     "enr:-MK4QDjZAzD165YMiCM6WrCdRN3EGDViJqP_v1fyOE-YuOYAQ102NnF8GjTJG_-yUUMwWKuEzRMJMIoObbgDB3jtZA6GAYCJeizfh2F0dG5ldHOIAAAAAAAAAACEZXRoMpCBQMXLYgAAcf__________gmlkgnY0gmlwhCJbpv2Jc2VjcDI1NmsxoQOGOP29TdRKCr40kT-ZgsvrMYIs6EIwuYGEq11txAj_VohzeW5jbmV0cwCDdGNwgjLIg3VkcIIu4A",
 	},
 	Validator: &ClientDetails{
 		DataVolume: "./data/validator_data",
