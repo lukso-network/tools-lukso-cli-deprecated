3f75244617cdd5bdda218675aabeca144fcf81da
Leonard Schellenberg <leonard.schellenberg@gmail.com>
2022-05-06 12:53:17 +0200

    added description

--- cmd/network_describe.go
+++ cmd/network_describe.go
@@ -12,13 +12,12 @@ import (
 // describeCmd represents the describe command
 var describeCmd = &cobra.Command{
 	Use:   "describe",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "describes the status of the network",
+	Long: `Describes the status of the network regarding PoS. The Beacon Api node can be chosen and the status perspective can be moved by giving an epoch number.
+Leaving out the epoch number gives the latest status.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		baseUrl, _ := cmd.Flags().GetString("beaconapi")
 		if baseUrl == "" {
