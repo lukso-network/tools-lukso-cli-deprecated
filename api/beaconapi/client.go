7fe070eb0f4b7b678844aad142c5390bc71750cc
Leonard Schellenberg <leonard.schellenberg@gmail.com>
2022-05-20 11:45:21 +0300

    network describe also on default beaconapi endpoint

--- api/beaconapi/client.go
+++ api/beaconapi/client.go
@@ -2,8 +2,6 @@ package beaconapi
 
 import "github.com/lukso-network/lukso-cli/api/http"
 
-const DefaultBeaconAPIEndpoint = "https://beacon.beta.l16.lukso.network" //  "34.90.85.198:3500"
-
 type BeaconClient struct {
 	client http.HttpClient
 }
