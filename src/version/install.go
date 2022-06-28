5b66dbd3ec89dd01cc519c56e84cb73d1500cc82
Thomas <hello@ando.codes>
2022-06-28 11:32:19 +0100

    cleanups

--- src/version/install.go
+++ src/version/install.go
@@ -5,14 +5,16 @@ import (
 	"fmt"
 	"github.com/hashicorp/go-getter"
 	"github.com/lukso-network/lukso-cli/src/utils"
-	"log"
+	"io"
 	"os"
 	"runtime"
+	"time"
 )
 
 const (
 	ReleaseURL = "https://github.com/lukso-network/lukso-cli/releases/download/%s/lukso-cli-%s-%s"
 	UserPath   = "/usr/local/bin"
+	TempPath   = "/tmp/"
 )
 
 func Install(version string) error {
@@ -21,20 +23,55 @@ func Install(version string) error {
 	if err != nil {
 		return err
 	}
+	err = os.MkdirAll(TempPath, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	tmpFile := TempPath + "/lukso_" + time.Now().String()
 	client := &getter.Client{
 		Ctx:  context.Background(),
 		Src:  downloadURL,
-		Dst:  UserPath + "/lukso",
-		Dir:  true,
+		Dst:  tmpFile,
+		Dir:  false,
 		Mode: getter.ClientModeFile,
 	}
 	if err = client.Get(); err != nil {
 		return err
 	}
+	err = os.Remove(UserPath + "/lukso")
+	if err != nil {
+		return err
+	}
+	err = copyFile(tmpFile, UserPath+"/lukso")
+	if err != nil {
+		return err
+	}
+	err = os.Remove(tmpFile)
+	if err != nil {
+		return err
+	}
 	err = os.Chmod(UserPath+"/lukso", 0755)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	utils.Coloredln(fmt.Sprintf("%s installed!", version))
 	return nil
+}
+
+func copyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
 }
